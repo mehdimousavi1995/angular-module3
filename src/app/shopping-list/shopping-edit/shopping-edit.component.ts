477b2edb75ba72910431715e2e5610b032638792
Mehdi Mousavi <mm.mousavi@rayanhamafza.com>
2019-11-04 18:14:17 +0330

    Adds logic for Add button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  // @ts-ignore
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onAddedItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
