58b0e2da2fc1f772761dfee6ffa811dfe4719418
Mehdi Mousavi <mm.mousavi@rayanhamafza.com>
2019-11-04 17:53:05 +0330

    Pass selected recipe to parent from child of child

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,9 +8,11 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is simply a test', 'http://localhost/images/recipe.jpeg'),
-    new Recipe('Test Recipe', 'This is simply a test', 'http://localhost/images/recipe.jpeg')
+    new Recipe('Test Recipe 1', 'This is simply a test 1', 'http://localhost/images/recipe.jpeg'),
+    new Recipe('Test Recipe 2', 'This is simply a test 2', 'http://localhost/images/recipe2.jpeg')
   ];
 
   constructor() {
@@ -19,4 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
